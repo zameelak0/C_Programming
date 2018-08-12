f66416545de060f498637207b7fad404af269ef6
zameelak0 <31796475+zameelak0@users.noreply.github.com>
2018-08-12 23:30:54 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,24 +3,60 @@
 
 int main()
 {
-    int a,b;
-    int myarray[5];
+    int a,b,c,d,matrix1[4][4],matrix2[4][4],matrix3[4][4],sum=0;
+    printf("Enter number of rows and column of matrix 1 \n");
+    scanf("%d%d",&a,&b);
 
-    for (int i=0;i<5;i++){
-        printf("Enter the elements  \n");
-        scanf("%d",&b);
-        myarray[i]=b;
-
-    }
-    printf("Enter element which u want to search \n");
-    scanf("%d",&a);
-    for (int j=0;j<5;j++){
-        if (a==myarray[j]){
-    printf("Element found at myarray[%d]=%d \n",j,a);
-   }else {
-
-   }
+    for (int i=0;i<a;i++){
+        for (int j=0;j<b;j++){
+            printf("Enter elements of matrix1[%d][%d]  \n",i,j);
+            scanf("%d",&matrix1[i][j]);
+        }
     }
 
+printf("Enter number of rows and column of matrix 2 \n");
+    scanf("%d%d",&c,&d);
+
+    for (int i=0;i<a;i++){
+        for (int j=0;j<b;j++){
+            printf("Enter elements of matrix2[%d][%d]  \n",i,j);
+            scanf("%d",&matrix2[i][j]);
+        }
+    }
+
+    printf("First matrix is \n");
+    for (int i=0;i<a;i++){
+        printf("\n");
+        for (int j=0;j<b;j++){
+            printf("%d ",matrix1[i][j]);
+        }
+    }
+
+    printf("Second matrix is \n");
+    for (int i=0;i<c;i++){
+        printf("\n");
+        for (int j=0;j<d;j++){
+            printf("%d ",matrix2[i][j]);
+        }
+    }
+
+
+        for (int i=0;i<a;i++){
+            for (int j=0;j<d;j++){
+                    matrix3[i][j]=sum;
+
+                for (int k=0;k<c;k++){
+                    sum=sum+matrix1[i][k]*matrix2[k][j];
+                }
+            }
+        }
+
+    printf("Product of matrix is \n");
+    for (int i=0;i<a;i++){
+            printf("\n");
+        for (int j=0;j<d;j++){
+            printf("%d ",matrix3[i][j]);
+        }
+    }
     return 0;
 }
